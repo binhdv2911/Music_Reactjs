eebe8a6610373d8e99350a7e7df48b82f9c3257c
binhdv2911 <binhdv2911@gmail.com>
2023-02-19 23:14:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Player from './components/Player'
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: "Forget me too ft. Halsey",
       artist: "Machine Gun Kelly",
       img_src: "./images/song-1.jpg",
-      src: "./music/Let-Me-Down-Slowly.mp3"
+      src: "./music/on-n-on.mp3"
     },
     {
       title: "Song 2",
-      artist: "Artist liar",
+      artist: "Artist 2",
       img_src: "./images/song-2.jpg",
-      src: "./music/Bad-Liar.mp3"
+      src: "./music/somebody-new.mp3"
     },
     {
       title: "Song 3",
-      artist: "Taylor Swift",
+      artist: "Artist 3",
       img_src: "./images/song-3.jpg",
-      src: "./music/Blank-Space.mp3"
+      src: "./music/on-n-on.mp3"
     },
     {
       title: "Song 4",
-      artist: "Taylor Swift",
+      artist: "Artist 4",
       img_src: "./images/song-4.jpg",
-      src: "./music/Look-What-You Made-Me-Do.mp3"
+      src: "./music/somebody-new.mp3"
     }
   ]);
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentSongIndex]);
 
   return (
     <div className="App">
-     <Player 
-     song={songs[currentSongIndex]}
-     nextSong = {songs[nextSongIndex]}/>
+      <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      />
     </div>
   );
 }
-
 export default App;
--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faPlay, faPause, faForward, faBackward} from '@fortawesome/free-solid-svg-icons'
-function PlayerControls() {
+import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons'
+
+function PayerControls(props) {
   return (
-    <div className="c-player--controls">
-      <button className='skip-btn'>
-        <FontAwesomeIcon icon={faBackward}/>
-      </button>
-      <button className='play-btn'>
-        <FontAwesomeIcon icon={faPlay}/>
-      </button>
-      <button className='skip-icon'>
-        <FontAwesomeIcon icon={faForward}/>
-      </button>
-    </div>
+      <div className="c-player--controls">
+          <button className="skip-btn" onClick={() => props.SkipSong(false)}>
+              <FontAwesomeIcon icon={faBackward} />
+          </button>
+          <button className="play-btn" onClick={() => props.setIsPlaying(!props.isPlaying)}>
+              <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} />
+          </button>
+          <button className="skip-btn" onClick={() => props.SkipSong()}>
+              <FontAwesomeIcon icon={faForward} />
+          </button>
+      </div>
   )
 }
 
-export default PlayerControls
+export default PayerControls
